a0adcfd7b2c67f452b0f4f4a51e51196ee1e0434
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-21 09:58:44 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,10 @@
-
+age_of_person = 2003 - current_age_for_birth_year.to_f
 def current_age_for_birth_year
-  puts 
+  puts "You are #{age_of_person} years old"
 end
+
+current_age_for_birth_year(1984)
+
+ age_of_person = current_age_for_birth_year(1984)
+
+    expect(age_of_person).to eq(19)
