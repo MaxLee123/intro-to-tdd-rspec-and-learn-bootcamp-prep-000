f646b123dc2ac89a2ee954013fd7a03d63191a47
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-21 10:09:57 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,10 @@
 current_year = 2003
 current_age_for_birth_year = 1984
 
-age_of_person = 2003 - current_age_for_birth_year.to_i
+def age_of_person
+age_of_person = current_year - current_age_for_birth_year.to_i
   puts "You are #{age_of_person} years old"
-
+end
 
 current_age_for_birth_year(1984)
 
